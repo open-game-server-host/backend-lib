8ff82aee25202eed172472f752c6f8f148530fc1
Dom <dominicraebeeson@gmail.com>
2026-01-29 18:33:22 +0000

    export everything individually :(

---
+++ src/auth/internalAuth.ts
@@ -0,0 +1,5 @@
+import { Request } from "express";
+
+export async function validateInternalRequest(req: Request) {
+    // TODO validate request
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export * from "./";
+export * from "./auth/internalAuth";
+export * from "./auth/userAuth";
+
+export * from "./config/appsConfig";
+export * from "./config/config";
+export * from "./config/globalConfig";
+
+export * from "./http/httpStatus";
+export * from "./http/httpUtils";
+
+export * from "./env";
+export * from "./error";
+export * from "./logger";
+export * from "./utils";
