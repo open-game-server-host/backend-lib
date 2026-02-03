249a2d8df1331e13b2f0d5711bc0dcdba194dbc0
Dom <dominicraebeeson@gmail.com>
2026-02-03 01:03:56 +0000

    downloadToVariable and downloadToFile functions

--- src/http/httpUtils.ts
+++ src/http/httpUtils.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { createWriteStream } from "node:fs";
 import { formatErrorResponseBody, getErrorHttpStatus, OGSHError } from "../error";
 
 export function expressErrorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
@@ -12,3 +13,88 @@ export function expressErrorHandler(error: Error, req: Request, res: Response, n
 }
 
 export type BodyRequest<Body = any> = Request<any, any, Body>;
+
+interface DownloadProgress {
+    bytesTotal: number;
+    bytesProcessed: number;
+}
+interface Download {
+    contentLength: number;
+    stream: ReadableStream;
+}
+export async function download(url: string, init?: RequestInit): Promise<Download> {
+    const response = await fetch(url, init);
+    if (!response.body) {
+        throw new OGSHError("general/unspecified", `no response body for url '${url}'`);
+    }
+
+    const contentLength = response.headers.get("Content-Length");
+    if (!contentLength) {
+        throw new OGSHError("general/unspecified", `missing Content-Length header, url '${url}'`);
+    }
+
+    const reader = response.body.getReader();
+    return {
+        contentLength: +contentLength,
+        stream: new ReadableStream({
+            start(controller) {
+                function push() {
+                    reader.read().then(({ done, value }) => {
+                        if (done) {
+                            controller.close();
+                            return;
+                        }
+
+                        controller.enqueue(value);
+                        push();
+                    });
+                }
+                push();
+            }
+        })
+    };
+}
+
+export async function downloadToString(url: string, init?: RequestInit, progressCallback?: (progress: DownloadProgress) => void): Promise<string> {
+    const response = await download(url, init);
+    const reader = response.stream.getReader();
+    const progress: DownloadProgress = {
+        bytesProcessed: 0,
+        bytesTotal: response.contentLength
+    }
+    const decoder = new TextDecoder();
+    let data = "";
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+            break;
+        }
+        data += decoder.decode(value);
+        if (progressCallback) {
+            progress.bytesProcessed = Math.min(progress.bytesTotal, progress.bytesProcessed + value.byteLength);
+            progressCallback(progress);
+        }
+    }
+    return data;
+}
+
+export async function downloadToFile(url: string, filePath: string, init?: RequestInit, progressCallback?: (progress: DownloadProgress) => void) {
+    const response = await download(url, init);
+    const reader = response.stream.getReader();
+    const progress: DownloadProgress = {
+        bytesProcessed: 0,
+        bytesTotal: response.contentLength
+    }
+    const writeStream = createWriteStream(filePath);
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+            break;
+        }
+        writeStream.write(value);
+        if (progressCallback) {
+            progress.bytesProcessed = Math.min(progress.bytesTotal, progress.bytesProcessed + value.byteLength);
+            progressCallback(progress);
+        }
+    }
+}
