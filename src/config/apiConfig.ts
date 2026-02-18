d9e1a7857a6e930befde81e2f77b7c2bb09da02e
Dom <dominicraebeeson@gmail.com>
2026-02-18 15:53:58 +0000

    api config

---
+++ src/config/apiConfig.ts
@@ -0,0 +1,24 @@
+import { getGlobalConfigBranch } from "../env";
+import { Config } from "./config";
+
+interface Api {
+    domain: string;
+}
+
+class ApiConfig extends Config<Api> {
+    constructor() {
+        super(
+            "Global",
+            "https://raw.githubusercontent.com/open-game-server-host",
+            "configs",
+            getGlobalConfigBranch(),
+            "api.json"
+        );
+    }
+}
+
+const apiConfig = new ApiConfig();
+
+export async function getGlobalConfig(): Promise<Api> {
+    return apiConfig.getConfig();
+}
