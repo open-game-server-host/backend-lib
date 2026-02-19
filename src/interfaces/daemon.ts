14c8b302014364a168ebc25508741a4fbcd38e5f
Dom <dominicraebeeson@gmail.com>
2026-02-19 14:39:55 +0000

    most of daemon is optional to accomodate first-time setup

--- src/interfaces/daemon.ts
+++ src/interfaces/daemon.ts
@@ -3,21 +3,21 @@ import { Region } from "./region";
 
 export interface Daemon {
     api_key_hash: string;
-    cpu_arch: string;
-    cpu_name: string;
     created_at: number;
     id: string;
-    ip: Ip;
-    os: string;
-    port_range_start: number;
-    port_range_end: number;
-    region: Region;
-    segments: number;
-    segments_available: number;
     setup_complete: boolean;
-    sftp_port: number;
-    url: string;
-    ws_url: string;
+    cpu_arch?: string;
+    cpu_name?: string;
+    ip?: Ip;
+    os?: string;
+    port_range_start?: number;
+    port_range_end?: number;
+    region?: Region;
+    segments?: number;
+    segments_available?: number;
+    sftp_port?: number;
+    url?: string;
+    ws_url?: string;
 }
 
 export interface RawDaemon extends DaemonData {
@@ -26,18 +26,18 @@ export interface RawDaemon extends DaemonData {
 
 export interface DaemonData {
     api_key_hash: string;
-    cpu_arch: string;
-    cpu_name: string;
     created_at: number;
-    ip_id: string;
-    os: string;
-    port_range_start: number;
-    port_range_end: number;
-    region_id: string;
-    segments: number;
-    segments_available: number;
-    setup_complete: boolean; // The api key needs to be generated and given to the daemon during its setup, which means a record is created before it's ready.
-    sftp_port: number;
-    url: string;
-    ws_url: string;
+    setup_complete: boolean;
+    cpu_arch?: string;
+    cpu_name?: string;
+    ip_id?: string;
+    os?: string;
+    port_range_start?: number;
+    port_range_end?: number;
+    region_id?: string;
+    segments?: number;
+    segments_available?: number;
+    sftp_port?: number;
+    url?: string;
+    ws_url?: string;
 }
