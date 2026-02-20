e2cbf41347ac76f98151c569b7fde0bc7461ff74
Dom <dominicraebeeson@gmail.com>
2026-02-20 12:58:02 +0000

    check token length

--- src/auth/userAuth.ts
+++ src/auth/userAuth.ts
@@ -7,9 +7,12 @@ export interface UserLocals {
 
 export async function userAuthMiddleware(req: Request, res: Response<any, UserLocals>, next: NextFunction) {
     if (!req.headers.authorization) {
-        throw new OGSHError("auth/invalid", `user auth middleware - 'authorization' header missing`);
+        throw new OGSHError("auth/invalid", `'authorization: bearer' header missing`);
     }
     const token = req.headers.authorization.substring(7);
+    if (token.length === 0) {
+        throw new OGSHError("auth/invalid", `malformed 'authorization: bearer' header`);
+    }
     res.locals.userId = token; // TODO authorise user properly, but for now the token is the userId for testing
     next();
 }
