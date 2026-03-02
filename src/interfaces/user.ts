96dee332ef62d1f3d46fcac2eb2cb40f8d78e94c
Dom <dominicraebeeson@gmail.com>
2026-03-02 16:44:33 +0000

    separate users and permissions

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,15 +2,4 @@ export interface User {
     id: string;
     authUid: string;
     createdAt: number;
-    permissions: UserPermission[];
 }
-
-export type UserPermission =
-    | "createContainer"
-
-    | "createDaemon"
-    | "listDaemons"
-    | "removeDaemon"
-
-    // TODO admin permissions
-;
