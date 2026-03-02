daea5fe177e47873b44ba22cf68d3472537ef137
Dom <dominicraebeeson@gmail.com>
2026-03-02 13:48:38 +0000

    ContainerPermission should live in the api

--- src/interfaces/container.ts
+++ src/interfaces/container.ts
@@ -19,23 +19,6 @@ export interface Container {
     versionId: string;
 }
 
-export type ContainerPermission =
-    | "start"
-    | "stop"
-    | "kill"
-    | "command"
-    | "install"
-    | "terminate"
-    | "setRuntime"
-    | "setName"
-    | "resize"
-    | "changeRegion"
-    | "makeBackup"
-;
-export interface ContainerWithPermissions extends Container {
-    userPermissions?: ContainerPermission[]; // If this is undefined, the user does not have any permission to access this container
-}
-
 export interface ContainerRegisterData extends ContainerAppData, ContainerPortsData {
     containerId: string;
     segments: number;
